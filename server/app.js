98f3d07627ec5d43951166dde4f2c1da376063f6
Sitripi0 <216026207+Sitripi0@users.noreply.github.com>
2025-07-28 21:44:21 +0200

    Initial setup REST API

--- server/app.js
+++ server/app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
-
+const cors = require("cors");
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
-
+const cohorts = require("./cohorts.json");
+const students = require("./students.json");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
@@ -15,6 +16,9 @@ const app = express();
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+app.use(cors({
+  origin: ["http://localhost:5173"]
+}));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
@@ -22,11 +26,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 
+
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
+});
+app.get("/api/cohorts",(req,res,next)=>{
+  res.json(cohorts);
+});
+app.get("/api/students",(req,res,next)=>{
+  res.json(students);
 });
 
 
